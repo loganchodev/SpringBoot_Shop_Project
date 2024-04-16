942ab79f84dcfa15e45689b8db4d2e8145095643
loganchodev <loganchodev@gmail.com>
2024-04-16 09:47:51 +0900

    thymeleaf 레이아웃 테스트

--- shop/src/main/java/com/shop/dto/ItemDto.java
+++ shop/src/main/java/com/shop/dto/ItemDto.java
@@ -1,2 +1,31 @@
-package com.shop.dto;public class ItemDto {
+package com.shop.dto;
+
+import com.shop.constant.ItemSellStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Lob;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private int price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
 }
